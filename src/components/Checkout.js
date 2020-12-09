aaf172aeb3f329f20b0ebddc0eb0af7840a2da13
Gaja Adhi <gajaadhi07@gmail.com>
2020-12-09 15:46:06 +0100

    deploy router

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,7 +3,7 @@ const Checkout = () => {
     return (
         <div className="checkout alert alert-success">
             <p>Votre commande est prise en compte avec succès.</p>
-            <a href="/" className="btn btn-light checkout-btn">Retour à la page d'accueil</a>
+            <a href="/crazy-artsy/" className="btn btn-light checkout-btn">Retour à la page d'accueil</a>
         </div>
     )
 }
